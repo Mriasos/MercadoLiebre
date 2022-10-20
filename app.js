5f3f151a92d383ab2c3cbbf56d287207c63b764a
Mriasos <maridalso@hotmail.com>
2022-10-20 16:08:31 -0300

    subiendo actualizcion

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 
-    const express = require('express');
-const app = express();
-
-const path = require('path');
-const publicPath = path.resolve('public');
-app.use(express.static(publicPath));
-
-const port = process.env.PORT ||3000
-const puerto = 1234;
-app.listen(port, ()=> {
-    console.log('iniciando')
-});
-
-app.get('/', (req, res)=>{
-    res.sendFile(path.resolve('views/home.html'))
-});
-app.get('/registro', (req, res)=>{
-    res.sendFile(path.resolve('views/register.html'))
-});
-
-app.get('/login', (req, res)=>{
-    res.sendFile(path.resolve('views/login.html'))
-});
+ const express = require('express');
+ const app = express();
+ const path = require('path');
+ 
+ const publicPath = path.resolve('public');
+ app.use(express.static(publicPath));
+ 
+ const puerto = process.env.PORT || 1234;
+ app.listen(puerto, () =>{
+     console.log("Servidor corriendo en el puerto http://localhost:1234/");
+ });
+ 
+ app.get('/', (req, res)=>{
+     res.sendFile(path.resolve('views/home.html'))
+ });
+ 
+ app.get('/registro', (req, res)=>{
+     res.sendFile(path.resolve('./views/register.html'))
+ });
+ 
+ app.get('/login', (req, res)=>{
+     res.sendFile(path.resolve('./views/login.html'))
+ });
+ 
